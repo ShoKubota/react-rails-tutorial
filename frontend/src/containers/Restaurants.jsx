eb59f158a33f4093ac17243736410802b7831b74
ShoKubota <sh2o.1114@mineo.jp>
2022-05-04 08:55:36 +0900

    add: topPage

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,11 +1,13 @@
-import React, {Fragment, useReducer, useEffect} from 'react';
+import React, { Fragment, useReducer, useEffect } from 'react';
 import styled from 'styled-components';
+// --- ここから追加 ---
+import { Link } from "react-router-dom";
+// --- ここまで追加 ---
 
-//Linkを入れて遷移できるようにする
-import { Link } from 'react-router-dom';
-
-//components
-import { Skeleton } from '@mui/material';
+// --- ここから追加 ---
+// components
+import Skeleton from '@material-ui/lab/Skeleton';
+// --- ここまで追加 ---
 
 // apis
 import { fetchRestaurants } from '../apis/restaurants';
@@ -14,16 +16,20 @@ import { fetchRestaurants } from '../apis/restaurants';
 import {
   initialState,
   restaurantsActionTypes,
-  restaurantsReducer
+  restaurantsReducer,
 } from '../reducers/restaurants';
 
+// --- ここから追加 ---
 // constants
 import { REQUEST_STATE } from '../constants';
+// --- ここまで追加 ---
 
 // images
 import MainLogo from '../images/logo.png';
 import MainCoverImage from '../images/main-cover-image.png';
+// --- ここから追加 ---
 import RestaurantImage from '../images/restaurant-image.jpg';
+// --- ここまで追加 ---
 
 const HeaderWrapper = styled.div`
   display: flex;
@@ -33,7 +39,7 @@ const HeaderWrapper = styled.div`
 
 const MainLogoImage = styled.img`
   height: 90px;
-`;
+`
 
 const MainCoverImageWrapper = styled.div`
   text-align: center;
@@ -43,6 +49,7 @@ const MainCover = styled.img`
   height: 600px;
 `;
 
+// --- ここから追加 ---
 const RestaurantsContentsList = styled.div`
   display: flex;
   justify-content: space-around;
@@ -68,23 +75,23 @@ const SubText = styled.p`
   color: black;
   font-size: 12px;
 `;
+// --- ここまで追加 ---
 
 export const Restaurants = () => {
-
   const [state, dispatch] = useReducer(restaurantsReducer, initialState);
 
   useEffect(() => {
     dispatch({ type: restaurantsActionTypes.FETCHING });
     fetchRestaurants()
     .then((data) =>
-      dispatch({ 
+      dispatch({
         type: restaurantsActionTypes.FETCH_SUCCESS,
         payload: {
           restaurants: data.restaurants
         }
       })
     )
-  },[])
+  }, [])
 
   return (
     <Fragment>
@@ -98,9 +105,9 @@ export const Restaurants = () => {
         {
           state.fetchState === REQUEST_STATE.LOADING ?
             <Fragment>
-              <Skeleton variant="rectangular" width={450} height={300} />
-              <Skeleton variant="rectangular" width={450} height={300} />
-              <Skeleton variant="rectangular" width={450} height={300} />
+              <Skeleton variant="rect" width={450} height={300} />
+              <Skeleton variant="rect" width={450} height={300} />
+              <Skeleton variant="rect" width={450} height={300} />
             </Fragment>
           :
             state.restaurantsList.map((item, index) =>

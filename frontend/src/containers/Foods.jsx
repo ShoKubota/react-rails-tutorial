6c0520143f396074526f29c161d0f8bd3a2a1933
ShoKubota <sh2o.1114@mineo.jp>
2022-05-04 21:06:20 +0900

    add: food.jsx

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,5 +1,13 @@
 import React, { Fragment, useEffect, useReducer } from 'react';
 
+import styled from 'styled-components';
+
+import { Link } from "react-router-dom";
+
+import { LocalMallIcon } from '../components/Icons';
+import { FoodWrapper } from '../components/FoodWrapper';
+import Skeleton from '@material-ui/lab/Skeleton';
+
 // reducers
 import {
   initialState as foodsInitialState,
@@ -10,8 +18,42 @@ import {
 //apis
 import { fetchFoods } from '../apis/foods'
 
+//images
+import MainLogo from '../images/logo.png';
+import FoodImage from '../images/food-image.jpg'
+
 // constants
+import { COLORS } from '../style_constants';
 import { REQUEST_STATE } from '../constants';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 32px;
+`;
+
+const BagIconWrapper = styled.div`
+  padding-top: 24px;
+`;
+
+const ColoredBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const FoodsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+`;
+
+const ItemWrapper = styled.div`
+  margin: 16px;
+`;
 
 export const Foods = ({match}) => {
   const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
@@ -29,20 +71,40 @@ export const Foods = ({match}) => {
   }, [])
   return (
     <Fragment>
+      <HeaderWrapper>
+        <Link to="/restaurants">
+          <MainLogoImage src={MainLogo} alt='main logo' />
+        </Link>
+        <BagIconWrapper>
+          <Link to='/orders'>
+            <ColoredBagIcon fontSize='large' />
+          </Link>
+        </BagIconWrapper>
+      </HeaderWrapper>
+      <FoodsList>
       {
         foodsState.fetchState === REQUEST_STATE.LOADING ?
         <Fragment>
-          <p>
-            ロード中...
-          </p>
+          {
+            [...Array(12).keys()].map(i =>
+              <ItemWrapper key={i}>
+                <Skeleton key={i} variant='rect' width={400} height={200} />
+              </ItemWrapper>
+            )
+          }
         </Fragment>
       :
         foodsState.foodsList.map(food =>
-          <div key={food.id}>
-            {food.name}
-          </div>
+          <ItemWrapper key={food.id}>
+            <FoodWrapper
+              food={food}
+              onClickFoodWrapper={(food) => console.log(food)}
+              imageUrl={FoodImage}
+            />
+          </ItemWrapper>
         )
       }
+      </FoodsList>
     </Fragment>
   )
 }
